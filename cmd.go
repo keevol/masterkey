f4e6832921a095f241f3ccd70d99e1be82ef9e52
Johnathan Howell <me@johnathanhowell.com>
2016-11-27 17:30:21 -0500

    add meta frontend, more meta testing

--- cmd.go
+++ cmd.go
@@ -73,7 +73,32 @@ var (
 			Usage:  "search [searchtext]: search the vault for locations containing searchtext",
 		}
 	}
+
+	addmetaCmd = func(v *vault.Vault) repl.Command {
+		return repl.Command{
+			Name:   "addmeta",
+			Action: addmeta(v),
+			Usage:  "addmeta [location] [meta name] [meta value]: add a metadata tag to the credential at [location]",
+		}
+	}
 )
+
+func addmeta(v *vault.Vault) repl.ActionFunc {
+	return func(args []string) (string, error) {
+		if len(args) != 3 {
+			return "", fmt.Errorf("addmeta requires 3 arguments. See help for usage.")
+		}
+		location := args[0]
+		metaname := args[1]
+		metaval := args[2]
+
+		if err := v.AddMeta(location, metaname, metaval); err != nil {
+			return "", err
+		}
+
+		return fmt.Sprintf("%v added to %v successfully.\n", metaname, location), nil
+	}
+}
 
 func search(v *vault.Vault) repl.ActionFunc {
 	return func(args []string) (string, error) {
@@ -169,7 +194,15 @@ func get(v *vault.Vault) repl.ActionFunc {
 			return "", err
 		}
 
-		return fmt.Sprintf("Username: %v\nPassword: %v\n", cred.Username, cred.Password), nil
+		printstring := fmt.Sprintf("Username: %v\nPassword: %v\n", cred.Username, cred.Password)
+
+		if len(cred.Meta) > 0 {
+			for metaname, metaval := range cred.Meta {
+				printstring += fmt.Sprintf("%v: %v\n", metaname, metaval)
+			}
+		}
+
+		return printstring, nil
 	}
 }
 
